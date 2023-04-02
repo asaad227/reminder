9023363f29d24cf764565d6f24de1c5ca72b90b0
asaad227 <a.saad227@yahoo.com>
2023-04-02 02:02:54 +0100

    update showtime function

--- index.js
+++ index.js
@@ -28,28 +28,12 @@ function userid(){
 }
 
 function showTime(){
-    var date = new Date();
-    var h = date.getHours();
-    var m = date.getMinutes();
-    var s = date.getSeconds()
-    var session = "AM";
-  
-    if(h === 0){
-      h = 12
-    }
-  
-    if(h > 12){
-      h = h - 12
-      session = "PM"
-    }
-  
-    h = (h < 10)? "0" + h : h;
-    m = (m < 10)? "0" + m : m;
-    s = (s < 10)? "0" + s : s;
-  
-    var time = `${h}:${m}:${s} ${session}`;
+    let currentDate = new Date();
+    let time= currentDate.toLocaleTimeString()
+    let date = currentDate.toDateString()
+
     const dateIn = document.querySelector(".dateTime");
-   dateIn.innerHTML = `${time}`;
+    dateIn.innerHTML = `${date} ${time}`;
   //  document.querySelector("#MyClockDisplay").textContent = `${time}`;
   
   
