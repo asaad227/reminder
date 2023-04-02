23657e589257a07bc0a0b1e317d6340169d47851
asaad227 <a.saad227@yahoo.com>
2023-04-02 01:46:38 +0100

    digital clock added

--- index.js
+++ index.js
@@ -27,12 +27,36 @@ function userid(){
     
 }
 
-function dated(){
+function showTime(){
+    var date = new Date();
+    var h = date.getHours();
+    var m = date.getMinutes();
+    var s = date.getSeconds()
+    var session = "AM";
+  
+    if(h === 0){
+      h = 12
+    }
+  
+    if(h > 12){
+      h = h - 12
+      session = "PM"
+    }
+  
+    h = (h < 10)? "0" + h : h;
+    m = (m < 10)? "0" + m : m;
+    s = (s < 10)? "0" + s : s;
+  
+    var time = `${h}:${m}:${s} ${session}`;
     const dateIn = document.querySelector(".dateTime");
-    dateIn.innerHTML = new Date();
-}
+   dateIn.innerHTML = `${time}`;
+  //  document.querySelector("#MyClockDisplay").textContent = `${time}`;
+  
+  
+   setTimeout(showTime, 1000)
+  }
 
-window.addEventListener("load", dated)
+window.addEventListener("load", showTime)
 btn.addEventListener("click", userid);
 
 
